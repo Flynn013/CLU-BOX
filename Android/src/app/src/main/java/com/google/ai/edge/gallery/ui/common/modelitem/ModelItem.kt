9752948759d13f4fe3dc82494ab2baa680499b06
Flynn013 <77566449+Flynn013@users.noreply.github.com>
2026-05-01 07:56:31 -0500

    Update ModelItem.kt

--- Android/src/app/src/main/java/com/google/ai/edge/gallery/ui/common/modelitem/ModelItem.kt
+++ Android/src/app/src/main/java/com/google/ai/edge/gallery/ui/common/modelitem/ModelItem.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -164,8 +164,10 @@ fun ModelItem(
                 modifier = Modifier.padding(top = 12.dp),
               )
             }
+          }
         }
-      }
+      } // <-- This is the bracket that was missing!
+      
       SharedTransitionLayout {
         AnimatedContent(isExpanded, label = "item_layout_transition") { targetState ->
           DownloadModelPanel(
